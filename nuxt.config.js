a8bd3c982bcafad0ba883e39abf1992bc383143e
Jovi <redzjovi@gmail.com>
2019-08-31 18:45:29 +0700

    [Nuxt7] install nuxt7.

---
+++ nuxt.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  // Nuxt modules
+  modules: [
+    'nuxt7',
+    '@nuxtjs/pwa'
+  ],
+
+  // PWA manifest
+  // https://github.com/nuxt-community/pwa-module
+  manifest: {
+    name: 'ningrat-catering--frontend--nuxt7',
+    description: ''
+  },
+
+  // Framework7 Config
+  framework7: {
+    // ...
+  },
+
+  // Build configuration
+  build: {
+    // Extract CSS in a separated file
+    extractCSS: true,
+
+    // You can extend webpack config here
+    extend (config) {
+      // ...
+    }
+  },
+
+  // Additional CSS configuration
+  css: [
+    'assets/app.css'
+  ]
+}
