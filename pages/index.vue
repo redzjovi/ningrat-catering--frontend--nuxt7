de20bc3821afff4cbe252f4065dfb7a44efee638
Jovi <redzjovi@gmail.com>
2019-09-29 08:57:33 +0700

    [Index] menu.

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,41 @@
       </f7-nav-left>
       <f7-nav-title>Home</f7-nav-title>
     </f7-navbar>
+    <f7-block-title>Menu</f7-block-title>
+    <f7-row v-if="menuLoading">
+      <f7-swiper :params="menuSwiperParams">
+        <f7-swiper-slide v-for="(product, index) in 10" :key="index">
+          <f7-card>
+            <div class="card-header index--menu--card--header skeleton-block skeleton-effect-blink"></div>
+            <f7-card-content>
+              <div class="skeleton-effect-blink skeleton-text">Product Name</div>
+              <div class="skeleton-effect-blink skeleton-text">Product Sell Price</div>
+            </f7-card-content>
+            <f7-card-footer>
+              <f7-skeleton-block class="skeleton-effect-blink" style="height: var(--f7-button-height); width: 100%;"></f7-skeleton-block>
+            </f7-card-footer>
+          </f7-card>
+        </f7-swiper-slide>
+      </f7-swiper>
+    </f7-row>
+    <f7-row v-else>
+      <f7-swiper :params="menuSwiperParams">
+        <f7-swiper-slide v-for="product in menuProducts" :key="product.id">
+          <f7-card>
+            <div @click="$f7router.navigate('/product/' + product.id + '/' + product.slug)" class="card-header index--menu--card--header" style="background-image: url(https://via.placeholder.com/200)"></div>
+            <f7-card-content>
+              <div>
+                <f7-link @click="$f7router.navigate('/product/' + product.id + '/' + product.slug)">{{ product.name }}</f7-link>
+              </div>
+              <div>Rp {{ product.sell_price }}</div>
+            </f7-card-content>
+            <f7-card-footer>
+              <f7-button @click="menuAddMethod(product.id)" class="index--menu--card--button--add" fill>Add</f7-button>
+            </f7-card-footer>
+          </f7-card>
+        </f7-swiper-slide>
+      </f7-swiper>
+    </f7-row>
     <f7-block>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos maxime incidunt id ab culpa ipsa omnis eos, vel excepturi officiis neque illum perferendis dolorum magnam rerum natus dolore nulla ex.</p>
       <p>Eum dolore, amet enim quaerat omnis. Modi minus voluptatum quam veritatis assumenda, eligendi minima dolore in autem delectus sequi accusantium? Cupiditate praesentium autem eius, esse ratione consequuntur dolor minus error.</p>
@@ -43,8 +78,56 @@
 
 <script>
 export default {
+  data() {
+    return {
+      menuLoading: true,
+      menuProducts: [],
+      menuSwiperParams: {
+        freeMode: true,
+        grabCursor: true,
+        keyboard: {
+          enabled: true,
+        },
+        slidesPerView: 2.2
+      }
+    }
+  },
+  created() {
+    this.getProductsMethod()
+  },
   components: {
     NavbarLeftLinkBars: () => import('~/components/NavbarLeftLinkBars')
+  },
+  methods: {
+    getProductsMethod() {
+      this.$axios.get('/product?active=1&limit=10')
+        .then(response => {
+          this.menuProducts = response.data.data
+          if (response.data.data.length <= 1) {
+            this.menuSwiperParams.slidesPerView = 1
+          }
+        })
+        .finally(() => {
+          this.menuLoading = false
+        })
+    },
+    menuAddMethod(id) {
+      this.$f7.dialog.preloader()
+      setTimeout(() => {
+        this.$f7.dialog.close()
+      }, 2000);
+    }
   }
 }
 </script>
+
+<style>
+.index--menu--card--button--add {
+  width: 100%;
+}
+.index--menu--card--header {
+  background-position: center;
+  height: 0;
+  padding-bottom: 100%;
+}
+</style>
